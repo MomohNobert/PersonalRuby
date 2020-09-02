7dea4d1b74bd30e5b6cbb3542bc725506550d88c
MomohNobert <okhai0112@gmail.com>
2020-09-02 16:26:27 +0100

    starting the basics

---
+++ working_with_strings.rb
@@ -0,0 +1,9 @@
+phrase = "   Giraffe Academy   "
+puts phrase
+puts phrase.upcase
+puts phrase.downcase
+puts phrase.strip
+puts phrase.length
+puts phrase.include? "Academy"
+puts phrase[8]
+puts phrase.index("G")
